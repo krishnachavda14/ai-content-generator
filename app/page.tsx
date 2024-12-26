98f7c200364e40e40fa5966b9cb1135477b4eaa3
krishnachavda14 <133307969+krishnachavda14@users.noreply.github.com>
2024-12-26 19:48:39 +0530

    updation

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,14 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
       <h2>Work in Progress...</h2>
       
       <h2>Click me!⬇️</h2>
       <Button>
       <a href="https://ai-content-generator-project.vercel.app/dashboard">GO TO DASHBOARD</a>
       </Button>
-=======
-      <h2>Work in Progress</h2>
-      <a href="https://ai-content-generator-project.vercel.app/dashboard">GO TO DASHBOARD</a>
       
->>>>>>> fb29d8fe3d8206743b1e366d1b8ceeb9b0b525c4
+      
     </div>
   );
 }
