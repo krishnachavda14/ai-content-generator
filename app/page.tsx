fb29d8fe3d8206743b1e366d1b8ceeb9b0b525c4
krishnachavda14 <133307969+krishnachavda14@users.noreply.github.com>
2024-12-26 15:09:32 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h2>HEELO</h2>
-      <Button>maaro mujhe</Button>
+      <h2>Work in Progress</h2>
+      <a href="https://ai-content-generator-project.vercel.app/dashboard">GO TO DASHBOARD</a>
+      
     </div>
   );
 }
